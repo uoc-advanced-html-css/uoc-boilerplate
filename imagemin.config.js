e04557774b310dd0753de73a65eb1686ef2751b3
Jordi Tarrida <jordi.tarrida@gmail.com>
2020-09-16 17:12:12 +0200

    fix imagemin config file

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 256 },
   "mozjpeg": { "progressive": true, "quality": 80 },
-  "pngquant": { "quality": [0.8] },
+  "pngquant": { "quality": [0.5, 0.8] },
   "svgo": {
     "plugins": [
       { "removeViewBox": false },
